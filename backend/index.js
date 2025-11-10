e7da39f60d87d9c8d095c0b9897d953abcf3cb99
Vaibhav Gupta <vaibhav0527gupta@gmail.com>
2025-11-10 22:30:03 +0530

    backendsetup

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,16 @@
-console.log('Backend server is running...');
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDb from "./config/db.js";
+const app=express();
+app.use(express.json());
+app.use(cors());
+
+dotenv.config();
+
+const PORT=process.env.PORT || 5000;
+app.listen(PORT,()=>{
+    console.log(`server is running on port ${PORT}`);
+     connectDb();
+})
